24badfa1ae2a09bb1968903426692a0178af10b0
Ethan Zemanick <ethanzem@buffalo.edu>
2023-03-30 17:05:01 -0400

    formatting changes to make css more legible

--- frontend/RPS15/src/Pages/CreateAccount.jsx
+++ frontend/RPS15/src/Pages/CreateAccount.jsx
@@ -4,28 +4,52 @@ const CreateAccount = () => {
   return (
     <div>
       <link rel="stylesheet" href="../src/Pages/CreateAccount.css" />
-      <div className="login-container">
-        <div className="flex-container" />
+      <div className="create-account-container">
         <div className="flex-item">
           <p className="logo">Account Create</p>
         </div>
+
         <form className="create-account-form">
           <div className="flex-item">
             <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username" className="form-input" required />
+            <input 
+            type="text" 
+            id="username" 
+            name="username" 
+            className="form-input" 
+            required />
           </div>
+
           <div className="flex-item">
             <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" className="form-input" required />
+            <input 
+            type="password" 
+            id="password" 
+            name="password" 
+            className="form-input" 
+            required />
           </div>
+
           <div className="flex-item">
             <label htmlFor="password-again">Password (again):</label>
-            <input type="password" id="password-again" name="password-again" className="form-input" required />
+            <input 
+            type="password" 
+            id="password-again" 
+            name="password-again" 
+            className="form-input" 
+            required />
           </div>
+
           <div className="flex-item">
             <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" className="form-input" required />
+            <input 
+            type="email" 
+            id="email" 
+            name="email" 
+            className="form-input" 
+            required />
           </div>
+
           <div className="flex-item">
             <button type="submit" className="form-button">Submit</button>
           </div>  
--- frontend/RPS15/src/Pages/Login.jsx
+++ frontend/RPS15/src/Pages/Login.jsx
@@ -4,50 +4,55 @@ import { useNavigate } from "react-router-dom"
 const Login = () => {
     const navigate = useNavigate();
     return (
-    <>
-     <link rel="stylesheet" href="../src/Pages/Login.css" />
-        <div className="login-container">
+        <div>
+            <link rel="stylesheet" href="../src/Pages/Login.css" />
+            <div className="login-container">
 
-            <div className="login-flex-container" />
-                <div className="flex-item">
-                <p className="logo">Account Login</p>
-                </div>
+                <div className="login-flex-container" />
 
-                <form className="login-form">
-                <div className="flex-item">
-                    <label htmlFor="username">Username:</label>
-                    <input
-                    type="text"
-                    id="username"
-                    name="username"
-                    className="form-input"
-                    required=""
-                    />
-                </div>
-                <div className="flex-item">
-                    <label htmlFor="password">Password:</label>
-                    <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    className="form-input"
-                    required=""
-                    />
-                </div>
-                <div className="flex-item button-flex-container">
-                    <button type="button" id="create-account" className="form-button" onClick={() => navigate('../CreateAccount')}>
-                    Create Account
-                    </button>
-                    <button type="submit" id="submit" className="form-button">
-                    Submit
-                    </button>
-                    <button type="submit" id="forgot-password" className="form-button">
-                    Forgot Password
-                    </button>
-                </div>
-                </form>
+                    <div className="flex-item">
+                        <p className="logo">Account Login</p>
+                    </div>
+
+                    <form className="login-form">
+                        <div className="flex-item">
+                            <label htmlFor="username">Username:</label>
+                            <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            className="form-input"
+                            required=""
+                            />
+                        </div>
+
+                        <div className="flex-item">
+                            <label htmlFor="password">Password:</label>
+                            <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            className="form-input"
+                            required=""
+                            />
+                        </div>
+
+                        <div className="flex-item button-flex-container">
+                            <button type="button" id="create-account" className="form-button" onClick={() => navigate('../CreateAccount')}>
+                            Create Account
+                            </button>
+
+                            <button type="submit" id="submit" className="form-button">
+                            Submit
+                            </button>
+
+                            <button type="submit" id="forgot-password" className="form-button">
+                            Forgot Password
+                            </button>
+                        </div>
+                    </form>
+            </div>
         </div>
-    </>
     )
 }
 
