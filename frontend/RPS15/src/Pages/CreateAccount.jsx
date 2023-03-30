1ecc1af435a40904846036f290126e3c9ed4b1fb
Ethan Zemanick <ethanzem@buffalo.edu>
2023-03-30 18:32:59 -0400

    okay now thats actually all the edits for the CSS for now

--- frontend/RPS15/src/Pages/CreateAccount.jsx
+++ frontend/RPS15/src/Pages/CreateAccount.jsx
@@ -5,56 +5,57 @@ const CreateAccount = () => {
     <div>
       <link rel="stylesheet" href="../src/Pages/CreateAccount.css" />
       <div className="create-account-container">
+        <div className="create-flex-container">
+          <div className="flex-item">
+            <p className="logo">Account Create</p>
+          </div>
 
-        <div className="flex-item">
-          <p className="logo">Account Create</p>
+          <form className="create-account-form">
+            <div className="flex-item">
+              <label htmlFor="username">Username:</label>
+              <input 
+              type="text" 
+              id="username" 
+              name="username" 
+              className="form-input" 
+              required />
+            </div>
+
+            <div className="flex-item">
+              <label htmlFor="password">Password:</label>
+              <input 
+              type="password" 
+              id="password" 
+              name="password" 
+              className="form-input" 
+              required />
+            </div>
+
+            <div className="flex-item">
+              <label htmlFor="password-again">Password (again):</label>
+              <input 
+              type="password" 
+              id="password-again" 
+              name="password-again" 
+              className="form-input" 
+              required />
+            </div>
+
+            <div className="flex-item">
+              <label htmlFor="email">Email:</label>
+              <input 
+              type="email" 
+              id="email" 
+              name="email" 
+              className="form-input" 
+              required />
+            </div>
+            <br/>
+            <div className="flex-item button-container">
+              <button type="submit" className="form-button">Submit</button>
+            </div>  
+          </form>
         </div>
-
-        <form className="create-account-form">
-          <div className="flex-item">
-            <label htmlFor="username">Username:</label>
-            <input 
-            type="text" 
-            id="username" 
-            name="username" 
-            className="form-input" 
-            required />
-          </div>
-
-          <div className="flex-item">
-            <label htmlFor="password">Password:</label>
-            <input 
-            type="password" 
-            id="password" 
-            name="password" 
-            className="form-input" 
-            required />
-          </div>
-
-          <div className="flex-item">
-            <label htmlFor="password-again">Password (again):</label>
-            <input 
-            type="password" 
-            id="password-again" 
-            name="password-again" 
-            className="form-input" 
-            required />
-          </div>
-
-          <div className="flex-item">
-            <label htmlFor="email">Email:</label>
-            <input 
-            type="email" 
-            id="email" 
-            name="email" 
-            className="form-input" 
-            required />
-          </div>
-          <br/>
-          <div className="flex-item">
-            <button type="submit" className="form-button">Submit</button>
-          </div>  
-        </form>
       </div>
     </div>
   );
