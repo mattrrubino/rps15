4546c1a321f41c400ed311bb381d602931f0c827
Ethan Zemanick <ethanzem@buffalo.edu>
2023-04-08 11:00:13 -0400

    account button redirects to login

--- frontend/RPS15/src/Pages/Home.jsx
+++ frontend/RPS15/src/Pages/Home.jsx
@@ -2,12 +2,20 @@ import './Home.css'
 import { useNavigate } from "react-router-dom"
 
 export default function Home() {
+  const navigate = useNavigate()
+
+  function onAccount() {
+    navigate('../login')
+  }
+
   return (
     <div className="home-div">
       <div className="home-container">
         <p className='flex-item title'>RPS-15</p>
         <p className='flex-item description'>Welcom to RPS-15, a modern take on the classic rock-paper-scissors game</p>
-        <button className='flex-item home-button'>Account</button>
+        <button className='flex-item home-button' onClick={onAccount}>
+          Account
+        </button>
         <br/>
         <button className='flex-item home-button'>Play</button>
         <br/>
