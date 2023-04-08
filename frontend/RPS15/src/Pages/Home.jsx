c6c11ae4bc9b01b295747d8cf58fcf09c807f138
Ethan Zemanick <ethanzem@buffalo.edu>
2023-04-08 10:51:51 -0400

    added home page and home page css

--- frontend/RPS15/src/Pages/Home.jsx
+++ frontend/RPS15/src/Pages/Home.jsx
@@ -1,5 +1,18 @@
+import './Home.css'
+import { useNavigate } from "react-router-dom"
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="home-div">
+      <div className="home-container">
+        <p className='flex-item title'>RPS-15</p>
+        <p className='flex-item description'>Welcom to RPS-15, a modern take on the classic rock-paper-scissors game</p>
+        <button className='flex-item home-button'>Account</button>
+        <br/>
+        <button className='flex-item home-button'>Play</button>
+        <br/>
+        <button className='flex-item home-button'>About</button>
+      </div>
+    </div>
   );
 }
