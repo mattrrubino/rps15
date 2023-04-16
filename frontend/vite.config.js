795bff3aa10a7dbb3e12caafd036236eb025919a
Matthew Rubino <mrrubino@buffalo.edu>
2023-04-16 15:16:36 -0400

    Fix development proxy config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
+      "/api/game": {
+        target: "ws://127.0.0.1:8000",
+        ws: true,
+        rewrite: () => "/game",
+      },
+
       "/api": {
         target: "http://127.0.0.1:8000",
         rewrite: (path) => path.substring(4),
