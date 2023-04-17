c12e1e74d18b28009eb1de00fc4aaffb4aa1c9b1
alin82 <89931455+alin82@users.noreply.github.com>
2023-04-17 15:35:12 -0400

    Profile

--- frontend/RPS15/src/Pages/Profile.jsx
+++ frontend/RPS15/src/Pages/Profile.jsx
@@ -1,7 +1,73 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import PieChart from '../components/PieChartForProfile';
+import './Profile.css';
+import { useState } from 'react';
 
 export default function Profile() {
+  const [playerData, setPlayerData] = useState([]);
+  const [eloMessage, setEloMessage] = useState("You're just ok, you still need to get better");
+
+
+  useEffect(() => {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/api/user");
+
+    console.log(xhr.status)
+
+    if (xhr.status === 200) {
+      xhr.responseType = 'json';
+      setPlayerData(xhr.response);
+    } 
+    // else {
+    //     alert("Failed to load your data")
+    // }
+    if (playerData["Elo"] < 500) {
+      setEloMessage("You really suck at this game, stop playing");
+    }
+
+    xhr.send();
+
+  })
+
+  let testdata = {
+    "rock": 3,
+    "scissors": 5,
+    "gun": 2,
+    "banana": 8,
+    "paper": 1,
+    "1": 4,
+    "2": 15,
+    "3": 3,
+    "zac": 60,
+    "5": 9,
+    "6": 3,
+    "7": 2,
+    "8": 1,
+    "9": 11,
+    "0": 20,
+  };
+
   return (
-    <div>Profile</div>
+    <div>
+      <div className='container'>
+        <div className='title'><span>You really suck at this game, stop playing</span></div>
+        {/* <div className='title'><span>{eloMessage}</span></div> */}
+        <div className='user'>
+          <h1>Username: Test</h1>
+          {/* <h1>Username: Test{playerData['Username']}</h1> */}
+        </div>
+      
+        <div className='chart'>
+          <h1>Your Stats:</h1>
+          <span>Wins: 0</span>
+          <span> Losses: 457</span>
+          <span> Elo: 13</span>
+          {/* <span>{playerData['Wins']}</span><span>{playerData['Losses']}</span><span>{playerData['Elo']}</span> */}
+          <PieChart data={testdata}/>
+          {/* <PieChart data={playerData['MoveCounts']}/> */}
+        </div>
+
+      </div>
+    </div>
   )
 }
---
+++ frontend/RPS15/src/components/PieChartForProfile.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { VictoryPie } from "victory-pie";
+
+// Takes one prop, data, which is a dictionary of the moves and how many times they've been used
+function App (props) {
+
+  function populateData (data) {
+    let Data = [];
+
+    for (const key in data){
+      Data.push({ x: key, y: data[key]})
+    }
+    
+    return Data;
+  }
+    
+  const style = {
+    data: {
+      fillOpacity: 0.9, stroke: "#45A29E", strokeWidth: 2
+    },
+    labels: {
+      fontSize: 15, fill: "#66FCF1"
+    }
+  }
+
+  return (
+    <div>
+      <VictoryPie
+        data={populateData(props.data)}
+        style={style}
+        colorScale="qualitative"
+        radius={150}
+      />
+    </div>
+  );
+};
+
+export default App;
